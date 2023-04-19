9502ee16bf1fbea6681294b01a6c09948fe446d7
Mayorova N <mayorova_nadezhda@inbox.ru>
2023-04-19 20:50:26 +0300

    Change-5

--- task_1/solution.js
+++ task_1/solution.js
@@ -27,7 +27,7 @@ function addInCartCountChange(value) {
 
     let newValue ;
     newValue = oldValue ;
-    newValue = newValue + "1" ;
+    newValue = oldValue + "1" ;
 
     // Конец решения задания №1.2.
 
